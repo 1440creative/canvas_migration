d9432d755578964dd621aa6bf1d7aab586b8d5d7
James Stubbs <stubbs@sfu.ca>
2025-08-06 10:19:47 -0700

    add type hints to utils/fs.py

--- utils/fs.py
+++ utils/fs.py
@@ -2,8 +2,9 @@
 
 import json
 from pathlib import Path
+from typing import Any, Dict
 
-def save_json(data: dict, filepath: Path) -> None:
+def save_json(data: Dict[str, Any], filepath: Path) -> None:
     filepath.parent.mkdir(parents=True, exist_ok=True)
     with filepath.open("w", encoding="utf-8") as f:
         json.dump(data, f, indent=2)
